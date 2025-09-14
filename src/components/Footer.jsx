f3c2789640d4ab1602d14400e7c4bb337d504b1c
MarkRaffy28 <markraffyromero28@gmail.com>
2025-09-14 10:52:37 +0800

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import geo_alt_fill from '../assets/geo_alt_fill.svg';
+import telephone_fill from '../assets/telephone_fill.svg';
+import envelope_fill from '../assets/../assets/envelope_fill.svg';
+import github from '../assets/github.svg';
+import facebook from '../assets/facebook.svg';
+import twitter from '../assets/twitter.svg';
+import telegram from '../assets/telegram.svg';
 
 function Footer() {
   return (
@@ -8,23 +15,44 @@ function Footer() {
       <Container>
         <Row>
           <Col md={3}>
-            <h5 className="fw-bold">About Us</h5>
-            <p>Short description about your company.</p>
+            <h5 className="fw-bold">About Me</h5>
+            <p>Just a 19-year-old IT boi.</p>
           </Col>
 
-          <Col md={3}>
-            <h5 className="fw-bold">Contact Us</h5>
+          <Col md={3} className="contacts">
+            <h5 className="fw-bold">Contacts</h5>
             <ul className="list-unstyled">
-              <li><a href="#"><i className="fa-solid fa-house"></i> 123 St., Manila</a></li>
-              <li><a href="#"><i className="fa-solid fa-phone"></i> (+63) 912-345-6789</a></li>
-              <li><a href="#"><i className="fa-solid fa-envelope"></i> gagrecipes@gmail.com</a></li>
+              <li>
+                <a href="#">
+                  <img src={geo_alt_fill} alt="geo_alt_fill" />
+                  Tinaan, Sta. Maria, Ilocos Sur
+                </a>
+                <a href="#">
+                  <img src={telephone_fill} alt="telephone_fill" />
+                  +63 999-757-8755
+                </a>
+                <a href="mailto: mark.inclusive1298@gmail.com">
+                  <img src={envelope_fill} alt="envelope_fill" />
+                  mark.inclusive1298@gmail.com
+                </a>
+              </li>
             </ul>
           </Col>
 
-          <Col md={3}>
-            <h5 className="fw-bold">Follow Us</h5>
-            <a href="#"><i className="fa-brands fa-facebook"></i></a>
-            <a href="#"><i className="fa-brands fa-instagram"></i></a>
+          <Col md={3} className="socials">
+            <h5 className="fw-bold">Socials</h5>
+            <a href="https://github.com/MarkRaffy28" className="github">
+              <img src="{gihub}" alt="github" />
+            </a>
+            <a href="https://www.facebook.com/markraffy.romero" className="facebook">
+              <img src="{facebook}" alt="facebook" />
+            </a>
+            <a href="https://x.com/MarkRaffy28" className="twitter">
+              <img src="{twitter}" alt="twitter" />
+            </a>
+            <a href="https://t.me/MarkRaffy28" className="telegram">
+              <img src="{telegram}" alt="telegram" />
+            </a>
           </Col>
 
           <Col md={3}>
@@ -37,7 +65,7 @@ function Footer() {
         </Row>
 
         <div className="text-center py-3 border-top mt-3">
-          © 2025 Grow a Garden Guide
+          © 2025 Mashu Personal Page
         </div>
       </Container>
     </footer>
