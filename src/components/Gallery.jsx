7a5cab9dfaf4b48c89534d36391444650ca11355
MarkRaffy28 <markraffyromero28@gmail.com>
2025-09-14 13:22:29 +0800

    update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,26 +1,16 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 import data from "../data.json";
 import './Gallery.css';
 
 function Gallery() {
-  let cardArr = data;
+  let cards = data;
   return (
     <div className="gallery-container">
-      {cardArr.map((eachCard) => (
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={"/images/" +eachCard.img+ ".jpg"} />
-          <Card.Body>
-            <Card.Title>{eachCard.title}</Card.Title>
-            <Card.Text>
-              {eachCard.text}
-            </Card.Text>
-            <Button variant="primary" href={(eachCard.link)}>Info</Button>
-          </Card.Body>
-        </Card>
+      {cards.map((card, index) => (
+        <div className={`card ${card.size}`}>
+          <img className="card-image" src={card.img} alt={`image_${index}`} />
+         </div>  
       ))}   
     </div>
   );
 }
 
-export default Gallery;
