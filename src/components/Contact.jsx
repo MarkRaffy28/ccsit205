0f1603c2e51bdddc2af09c946e46175bd895591b
MarkRaffy28 <markraffyromero28@gmail.com>
2025-09-14 14:51:37 +0800

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import { Form, Button } from "react-bootstrap";
 import './Contact.css';
 
 const Contact = () => {
@@ -46,6 +47,8 @@ const Contact = () => {
           Send
         </Button>
       </Form>
+      
+      <iframe className="m-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15230.718400338079!2d120.48013579138667!3d17.3791437361056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x338e6ad0806328bb%3A0x5f3d3a1caf86a4bb!2sTinaan%2C%20Santa%20Maria%2C%20Ilocos%20Sur!5e0!3m2!1sen!2sph!4v1757832536384!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
   );
 };
